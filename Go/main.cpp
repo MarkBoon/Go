2e1101d54fd0688ae7011099d47e490541154622
Mark Boon <mark_boon@me.com>
2013-06-06 17:49:53 -1000

    Initial Commit

---
+++ Go/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Go
+//
+//  Created by Mark Boon on 6/6/13.
+//  Copyright (c) 2013 Mark Boon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
